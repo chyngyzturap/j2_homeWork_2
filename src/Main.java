1f1ebe73eb837f620ffd0943304d2654bd2f0ad0
Chyngyz <chyngyzturapov@gmail.com>
2020-10-19 15:39:37 +0600

    up

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,6 @@ public class Main {
 
 
     }
-    public static Parent createObject1 (String className){
-        return switch className;
-        case "Child1" -> new Child1(7);
-        case "Child2" -> new Child2("Timur");
-        case "Child3" -> new Child3(176);
-        default -> null;
-
-
-    }
 
     public static void createObject (String className) throws Exception {
         switch (className) {
